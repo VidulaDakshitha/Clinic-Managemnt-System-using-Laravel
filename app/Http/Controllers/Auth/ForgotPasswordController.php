89d488833c02082a9e5e05cda89c19e029befba8
LakshanPerera <lakshan.perera713@gmail.com>
2019-09-15 03:42:56 +0530

    Fixed the ResetController

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -29,20 +29,4 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
-
-    /**
-     * Display the password reset view for the given token.
-     *
-     * If no token is present, display the link request form.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  string|null  $token
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     */
-    public function showResetForm(Request $request, $token = null)
-    {
-        return view('main.reset')->with(
-            ['token' => $token, 'email' => $request->email]
-        );
-    }
 }
