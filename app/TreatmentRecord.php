3461fb2d4430811dd609c982d56a0990151cf4d6
saranSR <46323366+saranSR@users.noreply.github.com>
2019-10-01 19:21:04 +0530

    changed

--- app/TreatmentRecord.php
+++ app/TreatmentRecord.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TreatmentRecord extends Model
 {
     public $timestamps = false;
+
+    protected $fillable = ['date','description'];
+    protected $primarykey = 'record_id';
 }
