b698d6ac123699c184b875930f23960034b92301
sathiralamal <sdsat756@gmail.com>
2019-10-01 11:07:27 +0530

    add method to get userpricrition

--- app/Http/Controllers/PatientPriscriptionOrderController.php
+++ app/Http/Controllers/PatientPriscriptionOrderController.php
@@ -50,16 +50,16 @@ class PatientPriscriptionOrderController extends Controller
         $id=Auth()->id();
 
         $userspricriptions = DB::table('prescriptions')
-                   ->where('prescriptions.patient_id',$id)
-                   ->join('doctors','doctors.doctor_id','prescriptions.doctor_id')
-                   ->join('prescribed_products','prescribed_products.prescription_id','prescriptions.id')
-                  // ->join('products', 'prescriptions.id', '=', 'products.prescription_id')
-                   ->join('products', 'prescribed_products.product_type_id', '=', 'products.product_id')
-                   ->select('products.name','products.product_id','products.selling_price', 'prescriptions.id','doctors.fullname')
-                  ->get();
+        ->where('prescriptions.patient_id',$id)
+        ->join('doctors','doctors.doctor_id','prescriptions.doctor_id')
+        ->join('prescribed_products','prescribed_products.prescription_id','prescriptions.id')
+       // ->join('products', 'prescriptions.id', '=', 'products.prescription_id')
+        ->join('products', 'prescribed_products.product_type_id', '=', 'products.product_id')
+        ->select('products.name','products.product_id','products.selling_price', 'prescriptions.id','doctors.fullname')
+       ->get();
 
-        
-        
+
+
             return view('product_order_system.UserPriscriptionView',['userspricriptions'=>$userspricriptions]);
     }
 
@@ -83,7 +83,7 @@ class PatientPriscriptionOrderController extends Controller
                    ->select('products.name','products.product_id','products.selling_price', 'prescriptions.id','doctors.fullname')
                    ->where($searchtype,'like','%'.$searchtxt.'%')
                    ->get();
-                   // dd($userspricriptions);
+      // dd($userspricriptions);
 
             return view('product_order_system.UserPriscriptionView',['userspricriptions'=>$userspricriptions]);
 
