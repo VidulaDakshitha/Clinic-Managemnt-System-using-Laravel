28c8c3447454fe93c83500cd3bc71be47f98bc87
saranSR <46323366+saranSR@users.noreply.github.com>
2019-10-01 22:06:18 +0530

    changed

--- resources/views/update_per.blade.php
+++ resources/views/update_per.blade.php
@@ -25,25 +25,25 @@
             @endif
           
             <div class="form-group">
-             Date of advice :<input type="date" class="form-control-file" name="date" value="{{$personal_records->date}}" required>
+             Date of advice :<input type="date" class="form-control" name="date" value="{{$personal_record->date}}" required>
            </div>
            
            <div class="form-group">
              <label>Patient_ID</label>
-             <input type="string" class="form-control" id="pid" name="patient_id" placeholder="Enter Patient_id" value="{{$personal_records->patient_id}}" >
+             <input type="string" class="form-control" id="pid" name="patient_id" placeholder="Enter Patient_id" value="{{$personal_record->patient_id}}" >
            </div>
 
            <div class="form-group">
              <label>Disease</label>
-             <textarea class="form-control" id="Disease" name="disease" rows="3"><{{$personal_records->disease}}></textarea>
+             <textarea class="form-control" id="Disease" name="disease" rows="3">{{$personal_record->disease}}</textarea>
            </div>
 
            <div class="form-group">
                <label>Description about disease</label>
-               <textarea class="form-control" id="Description" name="description" rows="3"><{{$personal_records->description}}></textarea>
+               <textarea class="form-control" id="Description" name="description" rows="3">{{$personal_record->description}}</textarea>
            </div>
 
-           <button type="submit" class="btn btn-primary">Submit</button>
+           <button type="submit" class="btn btn-primary">update</button>
            <button type="reset" class="btn btn-primary">Cancel</button>
            <a href="{{ url('/home_per') }}" class="btn btn-primary">Back</a>
          </fieldset>
