9187dba4efd7db9bb7fa22be92eda1fbb36823c8
LakshanPerera <lakshan.perera713@gmail.com>
2019-10-03 00:19:28 +0530

    updated nav

--- resources/views/backend/supplier/nav.blade.php
+++ resources/views/backend/supplier/nav.blade.php
@@ -1,5 +1,5 @@
 @section('nav-items')
 <li class="nav-item"><a class="nav-link" href="/supplier">Dashboard</a></li>
 <li class="nav-item"><a class="nav-link" href="/supplier/create">Create New Supplier</a></li>
-<li class="nav-item"><a class="nav-link" href="/supplier/generate-supplier-report">Generate Report</a></li>
+<li class="nav-item"><a class="nav-link" href="/generate-supplier-report">Generate Report</a></li>
 @endsection
