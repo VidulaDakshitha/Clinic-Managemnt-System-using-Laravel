c35d53fe55558401806f3522314bdf113ae43a2a
Kevin Gomez <kevgomez11@yahoo.com>
2019-10-04 20:19:25 +0530

    Kevin, gonna commit

--- resources/views/payment/paymentConfirm.blade.php
+++ resources/views/payment/paymentConfirm.blade.php
@@ -43,11 +43,8 @@
                             <div class="col-md-9 col-sm-9 col-xs-12">
                                 <div class="menu">
                                     <ul class="nav navbar-nav">
-                                        <li class="active"><a href="{{ url('paymentHome')}}">HOME</a></li>
-                                        <li><a href="#">Features</a></li>
-                                        <li><a href="#">Pricing</a></li>
-                                        <li><a href="#">Team</a></li>
-                                        <li><a href="#">Contact</a></li>
+                                        <li class="active"><a href="{{ url('/')}}">HOME</a></li>
+                                        
                                     </ul>
                                 </div>
                             </div>
