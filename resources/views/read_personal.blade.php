1dc030133334de0af28dd6a5771c572b6e1a8300
saranSR <46323366+saranSR@users.noreply.github.com>
2019-10-05 21:17:03 +0530

    modified some codes for good results

--- resources/views/read_personal.blade.php
+++ resources/views/read_personal.blade.php
@@ -1,4 +1,4 @@
-@extends('backend.layout')
+@extends('backend.l')
 @section('title', 'Record Management')
   
 <link rel="stylesheet" href={{ url('css/product/vendors/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}>
@@ -10,13 +10,14 @@
     <div class="col-md-6">
         <legend>Personal Record</legend>
         
-          ID:<p>{{$personal_record->record_id}}</p>
-          Date:<p>{{$personal_record->date}}</p>
-          Patient_id:<p>{{$personal_record->patient_id}}</p>
-          Disease:<p>{{$personal_record->disease}}</p>
-          Description about disease:<p>{{$personal_record->description}}</p>
+        ID:<p>{{$personal_record->record_id}}</p>
+        Date:<p>{{$personal_record->date}}</p>
+        Patient_id:<p>{{$personal_record->patient_id}}</p>
+        Disease:<p>{{$personal_record->disease}}</p>
+        Description about disease:<p>{{$personal_record->description}}</p>
+        
     </div>
-    <a href="/patient" class="btn btn-primary">Back</a>
+    <a href="{{ url('/patient') }}" class="btn btn-primary">Back</a>
 </div>
 
 @endsection
